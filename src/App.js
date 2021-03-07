ffa4bc9a069a741f206f49c92bb97b6341809446
PalmerAL <PalmerAL@users.noreply.github.com>
2021-03-06 18:11:28 -0600

    implement canvas component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+import DrawingCanvas from './canvasComponent.js'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <DrawingCanvas
+        width={500}
+        height={500}
+        lineWidth={10}
+        lineColor='darkorange'
+        image=''
+        onChange={(data) => { console.log(data.substring(0, 1000)) }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/canvasComponent.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+// https://remysharp.com/2010/07/21/throttling-function-calls
+function debounce (fn, delay) {
+  var timer = null
+  return function () {
+    var context = this; var args = arguments
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delay)
+  }
+}
+
+function CanvasComponent (props) {
+  let canvas = null
+
+  let callback = null
+  if (props.onChange) {
+    callback = debounce(function () {
+      props.onChange(canvas.toDataURL())
+    }, 250)
+  }
+
+  function onPointerMove (e) {
+    const ctx = canvas.getContext('2d')
+
+    const rect = canvas.getBoundingClientRect()
+
+    ctx.lineWidth = props.lineWidth || 4
+    ctx.strokeStyle = props.lineColor || 'black'
+    if (e.buttons === 1) {
+      ctx.lineTo((e.pageX - rect.left - window.scrollX) * 2, (e.pageY - rect.top - window.scrollY) * 2)
+      ctx.stroke()
+
+      callback()
+    } else {
+      ctx.closePath()
+      ctx.beginPath()
+    }
+  }
+
+  function insertImage (canvas) {
+    if (props.image) {
+      const img = new Image()
+      img.onload = function () {
+        canvas.getContext('2d').drawImage(img, 0, 0)
+      }
+      img.src = props.image
+    }
+  }
+
+  return (
+    <canvas
+      ref={(node) => {
+        canvas = node
+        insertImage(node)
+      }}
+      onPointerMove={onPointerMove}
+      width={props.width * 2}
+      height={props.height * 2}
+      style={{ background: 'white', width: props.width, height: props.height }}
+    />
+  )
+}
+
+export default CanvasComponent
